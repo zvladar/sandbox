72dc51192e8678a18dc430c2b49d7ebd61010780
zsigmond <Zsigmond Vladar>
2017-01-24 00:28:22 +0100

    Initial commit

---
+++ tests/unit/SaveUserTest.php
@@ -0,0 +1,17 @@
+<?php
+class UserTest extends \Codeception\Test\Unit
+{
+    public function testValidation()
+    {
+        $user = User::create();
+
+        $user->username = null;
+        $this->assertFalse($user->validate(['username']));
+
+        $user->username = 'toolooooongnaaaaaaameeee';
+        $this->assertFalse($user->validate(['username']));
+
+        $user->username = 'davert';
+        $this->assertFalse($user->validate(['username']));
+    }
+}
